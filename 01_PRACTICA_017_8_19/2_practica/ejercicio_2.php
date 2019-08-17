ee175447fb744fd2320166ec85ed52b42ba4b247
AntoniaWeb19 <54121958+AntoniaWeb19@users.noreply.github.com>
2019-08-17 13:12:55 -0400

    probando
    ejemplo con php

---
+++ 01_PRACTICA_017_8_19/2_practica/ejercicio_2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Practica 2</title>
+</head>
+<body>
+<h1>Practica 2</h1>
+     <h2>Modificar el programa anterior para que muestre tu direccion y tu numero de telefono.</h2>
+     <p>
+     <h3>mi nombre es</h3>
+        <?php
+            echo "Antonia";
+        ?>
+        <br>
+        <h4>mi direccion </h4>
+        <?php 
+            echo "Aveniada chacaltaya Nro.1444";
+        ?>
+        <br>
+        <h5>mi numero de telefono</h5>
+        <?php
+            echo "63180128";
+        ?>
+        <br>
+
+        </p>
+    
+</body>
+</html>
---
+++ 01_PRACTICA_017_8_19/3_practica/ejercicio_3.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Practica 3</title>
+</head>
+<body>
+<h1>Practica 3</h1>
+     <h2>Escribe un programa que muestre por pantalla 10 palabras en Ingles y Español.</h2>
+   <table>
+     <tr>
+        <th>Numero</th>
+        <th>Español</th>
+        <th>Ingles</th>
+        </tr>
+
+        <tr>
+        <td>1</td>
+        <td>Raton</td>
+        <td>Mouse</td>
+        </tr>
+
+        <tr>
+        <td>2</td>
+        <td>Teclado</td>
+        <td>Keyboard</td>
+        </tr>
+
+        <tr>
+        <td>3</td>
+        <td>Monitor</td>
+        <td>Monitor</td>
+        </tr>
+
+        <tr>
+        <td>4</td>
+        <td>Microfono</td>
+        <td>Microphone</td>
+        </tr>
+
+        <tr>
+        <td>5</td>
+        <td>Ordenador</td>
+        <td>Desktop Computer</td>
+        </tr>
+
+        <tr>
+        <td>6</td>
+        <td>Impresora</td>
+        <td>Printer</td>
+        </tr>
+
+        <tr>
+        <td>7</td>
+        <td>Unidada Otica</td>
+        <td>Optical Unit</td>
+        </tr>
+
+        <tr>
+        <td>8</td>
+        <td>Pantalla</td>
+        <td>Sreen</td>
+        </tr>
+
+        <tr>
+        <td>9</td>
+        <td>Camara Web</td>
+        <td>Webcam</td>
+        </tr>
+        
+        <tr>
+        <td>10</td>
+        <td>Disco Duro</td>
+        <td>Hard Drive</td>
+        </tr>
+       
+</body>
+</html>
